3ede08bb709a875ee882a36bb54125400097131c
Alex_Green <deadpoolweid@yandex.ru>
2016-02-24 17:34:32 +0300

    v.4.0.0
    - Теперь в окне результатов можно увидеть график введённой функции

--- PT/Result.Designer.cs
+++ PT/Result.Designer.cs
@@ -28,19 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Result));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.l_x = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(16, 96);
+            this.button1.Location = new System.Drawing.Point(105, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -50,7 +53,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(119, 96);
+            this.button2.Location = new System.Drawing.Point(208, 376);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -60,7 +63,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Location = new System.Drawing.Point(102, 312);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 2;
@@ -69,7 +72,7 @@
             // l_x
             // 
             this.l_x.AutoSize = true;
-            this.l_x.Location = new System.Drawing.Point(128, 32);
+            this.l_x.Location = new System.Drawing.Point(217, 312);
             this.l_x.Name = "l_x";
             this.l_x.Size = new System.Drawing.Size(12, 13);
             this.l_x.TabIndex = 3;
@@ -77,6 +80,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.chart1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.l_x);
             this.panel1.Controls.Add(this.button1);
@@ -84,14 +88,27 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(293, 154);
+            this.panel1.Size = new System.Drawing.Size(421, 440);
             this.panel1.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            this.chart1.Size = new System.Drawing.Size(421, 286);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            this.chart1.TextAntiAliasingQuality = System.Windows.Forms.DataVisualization.Charting.TextAntiAliasingQuality.SystemDefault;
             // 
             // Result
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 154);
+            this.ClientSize = new System.Drawing.Size(421, 440);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Result";
@@ -99,6 +116,7 @@
             this.Load += new System.EventHandler(this.Result_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,5 +128,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label l_x;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- PT/Result.cs
+++ PT/Result.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PT
 {
@@ -24,6 +25,25 @@ namespace PT
 
         private void Result_Load(object sender, EventArgs e)
         {
+
+            chart1.ChartAreas.Add(new ChartArea("Default"));
+
+            chart1.Series.Add(new Series("F(x)"));
+            chart1.Series["F(x)"].ChartArea = "Default";
+            chart1.Series["F(x)"].ChartType = SeriesChartType.Line;
+
+
+            int n = Convert.ToInt32(Math.Abs(Data.b - Data.a));
+            double[] axisXData = new double[n];
+            double[] axisYData = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                axisXData[i] = Data.a + i;
+                axisYData[i] = Data.f(Data.a + i);
+            }
+
+            chart1.Series["F(x)"].Points.DataBindXY(axisXData, axisYData);
+
             double _out = Output.main();
             if (double.IsNaN(_out))
             {
